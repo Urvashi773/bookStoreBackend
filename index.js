c314b5212af5d9a0c6005ab9180611a5c87b5c6c
Urvashi773 <erurvashiupadhyay@gmail.com>
2024-10-18 11:47:35 +0530

    resolved errors

--- index.js
+++ index.js
@@ -14,20 +14,20 @@ app.use(express.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const URI =process.env.mongoDBURI;
+const mongoURI =process.env.mongoDBURI;
+/* console.log(URI, "this is uri") */
 
 // connect to mongoDB
-try{
-    mongoose.connect(URI,{
-         useNewUrlParser:true,
-         useUnifiedTopology:true
-        
-    });
-   console.log("connected to mongoDB")
-}catch(error){
-    console.log("error",error)
-
-}
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+});
 
 //defining routes
 
