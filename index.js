6d3ad5461a1aed902577b5a62ef14480349835d5
Urvashi773 <erurvashiupadhyay@gmail.com>
2024-10-18 11:53:26 +0530

    final commit

--- index.js
+++ index.js
@@ -18,16 +18,19 @@ const mongoURI =process.env.mongoDBURI;
 /* console.log(URI, "this is uri") */
 
 // connect to mongoDB
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log("Connected to MongoDB");
-})
-.catch((error) => {
-    console.log("Error connecting to MongoDB:", error);
-});
+async function connectToDB() {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error);
+    }
+}
+
+connectToDB();
 
 //defining routes
 
